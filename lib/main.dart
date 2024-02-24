35272f083958bd807b75af05fa0ed0c81dd0e286
DenisRosalesUnah <dgrosales@unah.hn>
2024-02-24 16:10:25 -0600

    newChange

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Material Application',
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 3,
